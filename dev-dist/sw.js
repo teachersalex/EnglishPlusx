7f5f1f02336155e01b0cd765e5902a128da1cf30
teachersalex <alexsbd85@gmail.com>
2025-12-31 21:11:10 -0300

    Update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kfi3tb7ms2g"
+    "revision": "0.tnd8c6l1ai"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
